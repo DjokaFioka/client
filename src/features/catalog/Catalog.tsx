cfcf40243e384f5370900fd331ea83f1b0339930
Choopa <djordje.jokic@gmail.com>
2024-10-07 22:43:18 +0200

    Agent, Error and Not found page, loading, toasts, lint.

--- src/features/catalog/Catalog.tsx
+++ src/features/catalog/Catalog.tsx
@@ -1,16 +1,26 @@
-import axios from "axios";
+import agent from "../../app/api/agent";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
 import { useState, useEffect } from "react";
 
 export default function Catalog() {
     const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
+        agent.Catalog.list()
+            .then(products => setProducts(products))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false))
+        /*
         fetch('https://localhost:44353/api/products')
             .then(response => response.json())
             .then(data => setProducts(data))
+        */
     }, []) //if we forget to add the second parameter (the dependancy) this will execute forever. 
+
+    if (loading) return <LoadingComponent message="Loading products..." />
 
     return (
         <>
--- src/features/catalog/ProductDetails.tsx
+++ src/features/catalog/ProductDetails.tsx
@@ -1,8 +1,10 @@
 import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
+import agent from "../../app/api/agent";
+import NotFound from "../../app/errors/NotFound";
+import LoadingComponent from "../../app/layout/LoadingComponent";
 
 export default function ProductDetails() {
     const {id} = useParams<{id: string}>();
@@ -10,17 +12,27 @@ export default function ProductDetails() {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        axios.get(`https://localhost:44353/api/products/${id}`)
-            .then(response => setProduct(response.data))
+        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+        id && agent.Catalog.details(parseInt(id))
+            .then(response => setProduct(response))
             .catch(error => console.log(error))
             .finally(() => setLoading(false))
     }, [id])
 
+    /*
+    useEffect(() => {
+        //axios.get(`https://localhost:44353/api/products/${id}`)
+        id && agent.Catalog.details(parseInt(id))
+            .then(response => setProduct(response.data))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false))
+    }, [id])
+    */
     if (loading) 
-        return <h3>Loading...</h3>
+        return <LoadingComponent message="Loading product..." />
 
     if (!product) 
-        return <h3>Product not found</h3>
+        return <NotFound />
 
     return (
         <Grid container spacing={6}>
